a059fece7fe3000debc76b014a9bfc3f8d7da2b3
Brobo1 <armanahz95@gmail.com>
2024-06-19 16:37:51 +0200

    Add interface and functionality for custom grid generation
    This update introduces UI elements for specifying grid size and cell color. It adds the required HTML elements in index.html, CSS styling in style.css, and the implementation of grid generation and cell coloring logic in script.js. The grid can be customized in size, and cell colors can be manually chosen or randomly assigned.

---
+++ script.js
@@ -0,0 +1,54 @@
+const container = document.getElementById("grid-container");
+const gridInput = document.getElementById("grid-input");
+const gridSizeBtn = document.getElementById("grid-size-btn");
+const colorPicker = document.getElementById("color-picker");
+let cols;
+let cells;
+const gridSize = 32;
+const cellSize = 20;
+
+const createGrid = (gridSize, cellSize) => {
+  container.innerHTML = "";
+  for (let x = 0; x < gridSize; x++) {
+    cols = document.createElement("div");
+    cols.className = "cols";
+    for (let y = 0; y < gridSize; y++) {
+      cells = document.createElement("div");
+      cells.className = "cell";
+      cells.style.width = `${cellSize}px`;
+      cells.style.height = `${cellSize}px`;
+      cells.draggable = false;
+      cols.appendChild(cells);
+    }
+    container.appendChild(cols);
+  }
+};
+
+const random = () => {
+  return Math.floor(Math.random() * 255);
+};
+
+const randomColor = () => {
+  return `rgb(${random()},${random()},${random()})`;
+};
+
+createGrid(gridSize, cellSize);
+
+gridSizeBtn.addEventListener("click", () => {
+  if (gridInput.value !== "") {
+    createGrid(
+      gridInput.value,
+      Math.floor(cellSize * (gridSize / gridInput.value)),
+    );
+  } else {
+    alert("ENTER NOMBER BRUH");
+  }
+});
+
+container.addEventListener("mouseover", (e) => {
+  const target = e.target;
+  if (target.className === "cell" && e.buttons) {
+    if (target.style.backgroundColor === "")
+      target.style.backgroundColor = colorPicker.value;
+  }
+});
